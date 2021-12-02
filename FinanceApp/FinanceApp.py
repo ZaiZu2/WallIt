47f8fa7447147a155ed0a5daf952232481915141
ZaiZu2 <jakkawecki@gmail.com>
2021-12-02 23:22:14 +0100

    added CSV save/load functions

--- FinanceApp/FinanceApp.py
+++ FinanceApp/FinanceApp.py
@@ -1,15 +1,18 @@
 #! python 3
 
 import xml.etree.ElementTree as ET
-import copy, dataclasses, os
+import copy, dataclasses, os, csv
 import tkinter as tk
 from tkinter import filedialog
 
+class transactionTable:
+    """Data class cointaining a transaction"""
+
+    table = []
 
 class transaction:
-    """Data class cointaining a list of expenses"""
+    """Data class cointaining a transaction"""
 
-    expenses = []
     record = {
         "Name": None,
         "Title": None,
@@ -18,23 +21,15 @@ class transaction:
         "SrcAmount": [],
         "Date": None,
         "Place": None,
+        "Category": None,     
     }
 
     def __init__(self) -> None:
         pass
 
+def loadStatementXML():
+    """"Parsing expense data from Equabank XML monthly statement"""
 
-def fileTypeCheck(type):
-    while True:
-        path = filedialog.askopenfilename()
-
-        if os.path.basename(path).endswith(f"{type}"):
-            return path
-        else:
-            print("Wrong file type.")
-
-
-def loadStatementXML():  # parsing an XML monthly bank account statement
     def parseRecord(rootObj, XPath):
         try:
             value = rootObj.find(XPath, namespace).text.capitalize()
@@ -51,65 +46,72 @@ def loadStatementXML():  # parsing an XML monthly bank account statement
             value = None
         return value
 
-    path = fileTypeCheck(".xml")
-    tree = ET.parse(path)
+    # OPENING XML
+    while True:
+        XMLpath = filedialog.askopenfilename() # TODO: Opening file in fileTypeCheck???
+        if  os.path.basename(XMLpath).endswith(".xml"):
+            break
+    tree = ET.parse(XMLpath)
     root = tree.getroot()
 
     namespace = {"nms": "urn:iso:std:iso:20022:tech:xsd:camt.053.001.06"}
-    expenses = []
-    record = {
-        "Name": None,
-        "Title": None,
-        "Dir": None,
-        "Amount": [],
-        "SrcAmount": [],
-        "Date": None,
-        "Place": None,
-    }
+    
+    listOfExpenses = []
+    
+    # PARSING ENTRIES FROM XML
+    for i, entry in enumerate(root.findall(".//nms:Ntry", namespace)):
+        listOfExpenses.append(copy.deepcopy(transaction.record))
 
-    for count, i in enumerate(
-        root.findall(".//nms:Ntry", namespace)
-    ):  # Parsing expense records
-        expenses.append(copy.deepcopy(record))
+        listOfExpenses[i]["Name"] = parseRecord(entry, ".//nms:RltdPties//nms:Nm")
+        listOfExpenses[i]["Title"] = parseRecord(entry, ".//nms:Ustrd")
+        listOfExpenses[i]["Date"] = parseRecord(entry, ".//nms:BookgDt/nms:Dt")
+        listOfExpenses[i]["Place"] = parseRecord(entry, ".//nms:PstlAdr/nms:TwnNm")
 
-        expenses[count]["Name"] = parseRecord(i, ".//nms:RltdPties//nms:Nm")
-        expenses[count]["Title"] = parseRecord(i, ".//nms:Ustrd")
-        expenses[count]["Date"] = parseRecord(i, ".//nms:BookgDt/nms:Dt")
-        expenses[count]["Place"] = parseRecord(i, ".//nms:PstlAdr/nms:TwnNm")
+        listOfExpenses[i]["Amount"] = parseAmount(entry, "./nms:Amt")  # Parses array [Amount, Currency]
+        listOfExpenses[i]["SrcAmount"] = parseAmount(entry, ".//nms:InstdAmt/nms:Amt")  # Parses array [Amount, Currency]
 
-        expenses[count]["Amount"] = parseAmount(
-            i, "./nms:Amt"
-        )  # Parses array [Amount, Currency]
-        expenses[count]["SrcAmount"] = parseAmount(
-            i, ".//nms:InstdAmt/nms:Amt"
-        )  # Parses array [Amount, Currency]
-
-        expenses[count]["Dir"] = parseRecord(
-            i, "./nms:CdtDbtInd"
-        )  # Parsing just 'DBIT' or 'CRDT' to dictionary
-        if expenses[count]["Dir"] == "Dbit":
-            expenses[count]["Dir"] = -1
+        listOfExpenses[i]["Dir"] = parseRecord(entry, "./nms:CdtDbtInd")  # Parsing just 'DBIT' or 'CRDT' to dictionary
+        if listOfExpenses[i]["Dir"] == "Dbit":
+            listOfExpenses[i]["Dir"] = -1
         else:
-            expenses[count]["Dir"] = 1
+            listOfExpenses[i]["Dir"] = 1
 
     # PARSING CHECKS
-    # Check for correct expenses amounts to balance
-    incSum = round(
-        sum(expense["Dir"] * expense["Amount"][0] for expense in expenses), 2
-    )
+    # Check if expense sum equals parsed balance value
+    incSum = round(sum(expense["Dir"] * expense["Amount"][0] for expense in listOfExpenses), 2)
     if incSum == round(float(root.find(".//nms:TtlNtries/nms:Sum", namespace).text), 2):
-        print(f"Successfully loaded {len(expenses)} records.")
+        print(f"Successfully loaded {len(listOfExpenses)} records.")
     else:
         print("Records were loaded incorrectly.")
 
-    return expenses
+    return listOfExpenses
 
+
+def saveToCSV(records: transactionTable):
+    with open('save.csv', 'w', newline='') as saveFile:
+        writer = csv.DictWriter(saveFile, fieldnames = [key for key in transaction.record.keys()])
+        writer.writeheader()
+        writer.writerows(records)
+    print('Data successfully saved.')
+
+
+def loadFromCSV():
+    with open('save.csv', 'r', newline='') as saveFile:
+        reader = csv.DictReader(saveFile)  #fieldnames = [key for key in transaction.record.keys()]    
+        savedRecords = []
+        for row in reader:
+            savedRecords.append(row)
+
+    print('Data successfully loaded.')          
+    return savedRecords
 
 root = tk.Tk()
 root.withdraw()
 
-filePath = tk.filedialog.askopenfilename()
+dub = transactionTable()
+#dub.table.extend(loadStatementXML())
 
-loadStatementXML()
-
-ala = transaction()
+#saveToCSV(dub.table)
+dub.table.extend(loadFromCSV())
+for rec in dub.table:
+    print(rec)
