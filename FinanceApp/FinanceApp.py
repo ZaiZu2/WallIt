0352ff2bcf1c49749cb43ce88a8a9641b93c1d83
ZaiZu2 <jakkawecki@gmail.com>
2021-12-16 13:28:35 +0100

    Load/Save CSV methods corrected, classes corrected

--- FinanceApp/FinanceApp.py
+++ FinanceApp/FinanceApp.py
@@ -1,43 +1,78 @@
 #! python 3
 
 import xml.etree.ElementTree as ET
-import copy, dataclasses, os, csv
+import copy, os, csv, datetime
 import tkinter as tk
 from tkinter import filedialog
 from typing import Type, List, Optional
 
 # TODO: loadFromCSV loads strings of old data instead of different variables, which confuses appendWithoutDuplicates() which compares strings to different variables
+# TODO: change saveToCSV for proper headers, understand how it works, change row 
 
 class Transaction:
     """Class cointaining a transaction record"""
 
     def __init__(self) -> None:
+        self.name: str | None = None
+        self.title: str | None = None
+        self.dir: int | None =  None
+        self.amount: list = [None, None]
+        self.srcAmount: list = [None, None]
+        self.date: Optional[datetime.datetime] = None
+        self.place: Optional[str]  = None
+        self.category: Optional[str] = None
 
-        self.parameters = {
-            "Name": None,
-            "Title": None,
-            "Dir": None,
-            "Amount": [None, None],
-            "SrcAmount": [None, None],
-            "Date": None,
-            "Place": None,
-        }
+    def __repr__(self) -> str:
+        return f'{self.__class__.__qualname__}: {self.name}, {self.amount[0]}, {self.amount[1]}, {self.date}'
 
-        self.category = None
+    def __eq__(self, other):
+        if not isinstance(other, Transaction):
+            return TypeError
+        
+        return (self.name == other.name and 
+                self.title == other.title and
+                self.dir == other.dir and 
+                self.amount[0] == other.amount[0] and
+                self.amount[1] == other.amount[1] and
+                self.srcAmount[0] == other.srcAmount[0] and
+                self.srcAmount[1] == other.srcAmount[1] and
+                self.date == other.date and
+                self.place == other.place) 
 
+    def convertSavedRecord(self) -> None:
+        """Input-> "[129.0, 'CZK']" Output->[129.0 , 'CZK'] """
 
-    def categorizeRecord(self, category):
+        def convertAmounts(string):
+            tempAmount = []
+            tempCurrency = []
+            for character in string:
+                try:
+                    if character.isdigit() or character == '.':
+                        tempAmount.append(character)
+                    if character.isalpha():
+                        tempCurrency.append(character)
+                except ValueError:
+                    pass
+            return [float(''.join(tempAmount)), str(''.join(tempCurrency))]
+
+        if type(self.amount) is str: # check if amount is str and needs conversion
+            self.amount = convertAmounts(self.amount)
+        if (type(self.srcAmount) is str) and self.srcAmount: # check if 'amount is str & not empty' and needs conversion
+            self.srcAmount = convertAmounts(self.srcAmount)
+        #print(self.amount)
+        #print(self.srcAmount)
+
+    def categorizeRecord(self, targetCategory):
         """Assign a category to the record"""
 
         # ['salary', 'restaurant', 'sport', 'electronics', 'bill', 'activity', 'other']
-        self.category = category
+        self.category = targetCategory
 
 
 class TransactionTable:
     """Class cointaining a list of transactions"""
 
-    def __init__(self) -> None:
-
+    def __init__(self):
         self.table: list[Transaction] = []
 
 
@@ -47,14 +82,14 @@ class TransactionTable:
         for record in records:
             duplicate = False
             for checkedRecord in self.table:
-                if record.parameters == checkedRecord.parameters:
+                if record == checkedRecord:
                     duplicate = True
                     break
 
             if duplicate is False: 
                 self.table.append(record)
 
-
+    
     def saveToCSV(self) -> None:
         """Save transaction table to .CSV"""
 
@@ -63,35 +98,41 @@ class TransactionTable:
         # No clue why
         tempSaveTable: list = []
         for i, record in enumerate(self.table):
-            tempSaveTable.append(copy.deepcopy({}))
-            tempSaveTable[i] = record.parameters
+            tempSaveTable.append({})
+            tempSaveTable[i] = vars(record)
+
+        print(tempSaveTable)
+        
+        print(vars(self.table[0]).values())
+        lista = [items for items in vars(self.table[0])]
+        print(lista)
 
         with open('save.csv', 'w', newline='') as saveFile:
-            writer = csv.DictWriter(saveFile, fieldnames = [key for key in Transaction().parameters.keys()])
+            writer = csv.DictWriter(saveFile, fieldnames = [key for key in vars(self.table[0]).keys()])
             writer.writeheader()
             writer.writerows(tempSaveTable)
 
         print('Data successfully saved.')
-
-
+        
+    
     def loadFromCSV(self) -> None:
         """Load transaction table from .CSV"""
 
         try:
             with open('save.csv', 'r', newline='') as saveFile:
-                reader = csv.DictReader(saveFile)  #fieldnames = [key for key in transaction.record.keys()]
-
+                reader = csv.DictReader(saveFile)
                 recordTable = []
                 for i, row in enumerate(reader):
                     recordTable.append(Transaction())
-                    recordTable[i].parameters = row
-
-            self.appendWithoutDuplicates(recordTable)        
-            print('Data successfully loaded.')
+                    recordTable[i].__dict__ = row
+                    recordTable[i].convertSavedRecord()
         
         except FileNotFoundError:
             print('Savefile not found.')
 
+        self.appendWithoutDuplicates(recordTable)        
+        print('Data successfully loaded.')
+        
 
     def loadStatementXML(self):  # parsing an XML monthly bank account statement
         def parseRecord(rootObj, XPath):
@@ -109,7 +150,15 @@ class TransactionTable:
             except AttributeError:
                 value = None
             return value
-
+        '''
+        def parseDate(rootObj, XPath):
+            try:
+                date = 
+                value.append(float(rootObj.find(XPath, namespace).text)) 
+            except AttributeError:
+                value = None
+            return value        
+        '''
         path = fileTypeCheck(".xml")
         tree = ET.parse(path)
         root = tree.getroot()
@@ -119,23 +168,23 @@ class TransactionTable:
         for count, i in enumerate(root.findall(".//nms:Ntry", namespace)):  # Parsing transaction records
             expenses.append(copy.deepcopy(Transaction()))
 
-            expenses[count].parameters["Name"] = parseRecord(i, ".//nms:RltdPties//nms:Nm")
-            expenses[count].parameters["Title"] = parseRecord(i, ".//nms:Ustrd")
-            expenses[count].parameters["Date"] = parseRecord(i, ".//nms:BookgDt/nms:Dt")
-            expenses[count].parameters["Place"] = parseRecord(i, ".//nms:PstlAdr/nms:TwnNm")
+            expenses[count].name = parseRecord(i, ".//nms:RltdPties//nms:Nm")
+            expenses[count].title = parseRecord(i, ".//nms:Ustrd")
+            expenses[count].date = parseRecord(i, ".//nms:BookgDt/nms:Dt")
+            expenses[count].place = parseRecord(i, ".//nms:PstlAdr/nms:TwnNm")
 
-            expenses[count].parameters["Amount"] = parseAmount(i, "./nms:Amt")  # Parses list [Amount, Currency]
-            expenses[count].parameters["SrcAmount"] = parseAmount(i, ".//nms:InstdAmt/nms:Amt")  # Parses list [Amount, Currency]
+            expenses[count].amount = parseAmount(i, "./nms:Amt")  # Parses list [Amount, Currency]
+            expenses[count].srcAmount = parseAmount(i, ".//nms:InstdAmt/nms:Amt")  # Parses list [Amount, Currency]
 
-            expenses[count].parameters["Dir"] = parseRecord(i, "./nms:CdtDbtInd")  # Parsing just 'DBIT' or 'CRDT' to a dictionary
-            if expenses[count].parameters["Dir"] == "Dbit":
-                expenses[count].parameters["Dir"] = -1
+            expenses[count].dir = parseRecord(i, "./nms:CdtDbtInd")  # Parsing just 'DBIT' or 'CRDT' to a dictionary
+            if expenses[count].dir == "Dbit":
+                expenses[count].dir = -1
             else:
-                expenses[count].parameters["Dir"] = 1
+                expenses[count].dir = 1
 
         # PARSING CHECKS
         # Check for correct expenses amounts to balance
-        incSum = round(sum(expense.parameters["Dir"] * expense.parameters["Amount"][0] for expense in expenses), 2)
+        incSum = round(sum(expense.dir * expense.amount[0] for expense in expenses), 2)
         if incSum == round(float(root.find(".//nms:TtlNtries/nms:Sum", namespace).text), 2):
             print(f"Successfully loaded {len(expenses)} records.")
         else:
@@ -161,9 +210,11 @@ def main():
 
     print(len(table.table))
     table.loadFromCSV()
+
+    #table.loadStatementXML()
     print(len(table.table))
-    table.loadStatementXML()
-    print(len(table.table))
+    print(repr(table.table[10]))
+    #table.saveToCSV()
     root = tk.Tk()
     root.withdraw()
 
@@ -171,7 +222,7 @@ def main():
     #table.loadStatementXML()
     #[print(table.n.parameters) for table.n in table.table]
 
-    table.saveToCSV()
+    #table.saveToCSV()
 
 
 main()
