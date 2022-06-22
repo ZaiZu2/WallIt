29aedc333890ffe8ffcdb377981c0926c33a2d53
ZaiZu2 <jakkawecki@gmail.com>
2022-06-22 22:50:39 +0200

    bugfix/new: fully functional dynamic login submenus with session memory

--- FinanceApp/static/js/login.js
+++ FinanceApp/static/js/login.js
@@ -1,36 +1,27 @@
-const toggleWindow = function (divId) {
-  const menuNode = document.querySelector(divId);
-  menuNode.classList.toggle('hidden');
+// show specified submenu while hiding the rest
+const showSubmenu = function (submenuId) {
+  const submenuNodes = document.getElementsByClassName('sub-menu');
+
+  [...submenuNodes].forEach(submenuNode => {
+    if (submenuNode.id === submenuId) {
+      submenuNode.classList.remove('hidden');
+      // save last open submenu
+      sessionStorage.setItem('lastOpenSubmenu', submenuNode.id);
+    }
+    else submenuNode.classList.add('hidden');
+  })
 }
 
-const backButtons = document.querySelectorAll('#back');
-backButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    toggleWindow('#login');
-
-    let buttonParent = button.parentElement;
-    while (true) {
-      if (buttonParent.id === 'reset-password')
-      {
-        toggleWindow('#reset-password');
-        break;
-      } else if (buttonParent.id === 'sign-up') {
-        toggleWindow('#sign-up');
-        break;
-      };
-      buttonParent = buttonParent.parentElement;
-    };
-  });
+const backButtons = document.getElementsByClassName('back-button');
+[...backButtons].forEach((button) => {
+  button.addEventListener('click', () => showSubmenu('login'));
 });
 
-const resetPassButton = document.querySelector('#reset-password-menu');
-resetPassButton.addEventListener('click', () => {
-    toggleWindow('#login');
-    toggleWindow('#reset-password');
-});
+const resetPassButton = document.getElementById('reset-password-button');
+resetPassButton.addEventListener('click', () => showSubmenu('reset-password'));
 
-const signUpButton = document.querySelector('#sign-up-menu');
-signUpButton.addEventListener('click', () => {
-    toggleWindow('#login');
-    toggleWindow('#sign-up');
-});
+const signUpButton = document.getElementById('sign-up-button');
+signUpButton.addEventListener('click', () => showSubmenu('sign-up'));
+
+// Load last open submenu when page gets refreshed
+showSubmenu(sessionStorage.getItem('lastOpenSubmenu'))
